2b83ae143c0d061315e045110a3b83915fbf7260
goodtimeslim <bennyska@gmail.com>
2017-03-11 10:39:22 -0800

    Add alternating colors in list box

--- pyside.py
+++ pyside.py
@@ -9,9 +9,7 @@ import PySide.QtCore as qc
 
 class QCustomThread(qc.QThread):
 
-
     def __init__(self):
-        # super(QCustomThread, self).__init__()
         qc.QThread.__init__(self)
         self.previous_value = ''
         self.new_value = ''
@@ -20,14 +18,11 @@ class QCustomThread(qc.QThread):
         self.new_value = pyperclip.paste()
         if self.new_value != self.previous_value:
             self.previous_value = self.new_value
-            print(self.previous_value)
             return self.previous_value
 
     def run(self):
-        print('running')
         while True:
             x = self.get_string()
-            print('running')
             if x:
                 self.emit(qc.SIGNAL('add_value(QString)'), x)
             time.sleep(0.1)
@@ -37,17 +32,16 @@ class Example(QtGui.QWidget):
     def __init__(self):
         super(Example, self).__init__()
         self.list = qt.QListWidget(self)
+        self.list.setAlternatingRowColors(True)
         self.initUI()
         self.start()
 
     def initUI(self):
-        print('initUI')
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('Icon')
         self.setWindowIcon(QtGui.QIcon('web.png'))
 
     def start(self):
-        print('start')
         self.thread = QCustomThread()
         self.connect(self.thread, qc.SIGNAL('add_value(QString)'), self.add_value)
         self.thread.start()
@@ -55,7 +49,7 @@ class Example(QtGui.QWidget):
     def add_value(self, clip):
 
         new_item = qt.QListWidgetItem(clip)
-        self.list.addItem(new_item)
+        self.list.insertItem(0, new_item)
 
 
 def main():
