30ddad3f27d39b7f28718d1705e0a281e37a4499
goodtimeslim <bennyska@gmail.com>
2017-03-10 17:09:29 -0800

    Paste selected entry

--- scratch.py
+++ scratch.py
@@ -4,10 +4,12 @@ import pyperclip
 import time
 
 root = tk.Tk()
+
 scrollbar = tk.Scrollbar(root)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 listbox = tk.Listbox(root)
 listbox.pack()
+
 
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
@@ -21,6 +23,12 @@ def get_input():
         recent_value = new_value
         listbox.insert(tk.END, new_value)
     root.after(100, get_input)
+
+def get_item(evt):
+    value = listbox.get(listbox.curselection())
+    pyperclip.copy(value)
+
+listbox.bind('<<ListboxSelect>>', get_item)
 
 root.after(100, get_input)
 
