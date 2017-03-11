8b1618ffd6c81be54c70218c9e3327690cb427f6
goodtimeslim <bennyska@gmail.com>
2017-03-10 17:53:04 -0800

    Change layout of buttons

--- scratch.py
+++ scratch.py
@@ -5,14 +5,15 @@ from tkinter import font
 
 root = tk.Tk()
 
-scrollbar = tk.Scrollbar(root)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+# scrollbar = tk.Scrollbar(root)
+# scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 listbox = tk.Listbox(root, font= font.Font(size=12))
-listbox.pack()
+listbox.grid(row=0, column=0)
+# listbox.pack()
 
 
-listbox.config(yscrollcommand=scrollbar.set)
-scrollbar.config(command=listbox.yview)
+# listbox.config(yscrollcommand=scrollbar.set)
+# scrollbar.config(command=listbox.yview)
 
 recent_value = ''
 def get_input():
@@ -30,13 +31,15 @@ def get_item(evt):
 listbox.bind('<<ListboxSelect>>', get_item)
 
 clear_button = tk.Button(root, text='Clear list', command=lambda lb=listbox: lb.delete(0, tk.END))
-clear_button.pack()
+clear_button.grid(row=0, column=1)
+# clear_button.pack()
 
 def close_window():
     root.destroy()
 
 exit_button = tk.Button(root, text='Exit', command=close_window)
-exit_button.pack()
+exit_button.grid(row=1, column=1)
+# exit_button.pack()
 
 
 root.after(100, get_input)
